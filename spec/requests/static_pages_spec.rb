d254e9a5f9c962e6eaeba893098ffcba38d7e4f7
Noelia Rodriguez Martin <alu0100724622@ull.edu.es>
2014-03-26 11:57:58 +0000

    avanzando con los css

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,4 +26,17 @@ describe "Help page" do
       visit '/static_pages/home'
       expect(page).not_to have_title('| Home')
     end
+
+describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
   end
+end
+end
