6ea97b60fae402c557e7cb1d63a667d7866443c0
Noelia Rodriguez Martin <alu0100724622@ull.edu.es>
2014-03-26 10:33:39 +0000

    bootstarp instalado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+P6::Application.config.secret_key_base = 'a0377f0539452d54d3b8658ef855cbf920227635a7811e5f4f68a289f04d5880320a458dad715fb2defa4d47eebff2dd340f2112f9644885d6408d0c1067e4b5'
